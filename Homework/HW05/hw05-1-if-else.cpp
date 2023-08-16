43f6c6a31ef0b3af2b4853db95a92f56acbb0d6b
Chonlasak Tumna <137064083+ChonlasakThamana@users.noreply.github.com>
2023-08-16 13:48:34 +0700

    5-1

--- Homework/HW05/hw05-1-if-else.cpp
+++ Homework/HW05/hw05-1-if-else.cpp
@@ -11,3 +11,15 @@
     Output:
         Result: D C B A
 */
+#include <stdio.h>
+
+int main() {
+    char chars[4];
+    
+    printf("กรอกค่า 4 ตัวอักขระ: ");
+    scanf("%c %c %c %c", &chars[0], &chars[1], &chars[2], &chars[3]);
+    
+    printf("ผลลัพธ์: %c %c %c %c\n", chars[3], chars[2], chars[1], chars[0]);
+    
+    return 0;
+}
